6a95053a4c4125d897f4c846ad5929465d5615e7
Navjot Kaur <navjot.kaur@ensuesoft.com>
2023-09-15 16:21:49 +0530

    Navbar fixes

--- src/components/CustomFrame/index.tsx
+++ src/components/CustomFrame/index.tsx
@@ -4,7 +4,7 @@ const ipcRenderer =
     : false;
 const CustomFrame = () => {
   return (
-    <nav className="d-flex text-white justify-content-between align-items-center p-3 border-bottom">
+    <nav className="d-flex text-white justify-content-between align-items-center p-3 border-bottom pe-0">
       <div className="left-nav">
         <span><img
           src="./small-logo.ico"
@@ -15,7 +15,7 @@ const CustomFrame = () => {
       </div>
       <div className="right-nav">
         <span className="nav-link" onClick={() => ipcRenderer.send("minimizeWindow", {})}><i className="fas fa-minus"></i></span>
-        <span className="nav-link pe-0" onClick={() => ipcRenderer.send("closeWindow", {})}><i className="fas fa-times"></i></span>
+        <span className="nav-link" onClick={() => ipcRenderer.send("closeWindow", {})}><i className="fas fa-times"></i></span>
       </div>
     </nav>
   );
